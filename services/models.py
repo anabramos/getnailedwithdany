2ad4e6d16784c6bcab67877bcba0d2e9d7c7ca17
anabramos <anaramosbarretto@gmail.com>
2022-03-06 08:08:41 +0000

    Add Service Model to Admin panel

--- services/models.py
+++ services/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Service(models.Model):
+    # Model for item of service provided
+    service_id = models.AutoField(primary_key=True)
+    service_title = models.CharField(max_length=50, unique=True)
+    service_description = models.CharField(max_length=200, unique=True)
+    service_price = models.FloatField()
+
+    class Meta:
+        ordering = ['service_title']
+    
+    def __str__(self):
+        return self.service_title
