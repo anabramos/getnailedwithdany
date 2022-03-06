9834a4a3dd3e710c0a4c9b25bb020ed7c153d385
anabramos <anaramosbarretto@gmail.com>
2022-03-06 09:05:22 +0000

    Create Customer Model and connect to Admin panel

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    # Model for customer details
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=50)
+    customer_email = models.EmailField(max_length=254, default="", unique=True)
+
+    def __str__(self):
+        # return customer name
+        return self.customer_name
