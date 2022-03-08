37ea5c16f9e6b5a45c7e221d074d39568f13b2c9
anabramos <anaramosbarretto@gmail.com>
2022-03-08 18:01:25 +0000

    Install allauth app package

--- getnailed/urls.py
+++ getnailed/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('website.urls'), name='website_urls'),
-    path('services/', include('services.urls'), name='services_urls')
+    path('services/', include('services.urls'), name='services_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
