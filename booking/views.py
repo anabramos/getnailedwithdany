ecd64b993085d4c19da8fbb8e8d045a1f7d8a0f9
anabramos <anaramosbarretto@gmail.com>
2022-03-11 14:47:13 +0000

    Create template view for make appointment form

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import AppointmentForm
 
 # Create your views here.
+
+
+def make_appointment(request):
+    """
+    Return Make Appointment form rendered in new page
+    """
+    appointment_form = AppointmentForm()
+    return render(request, 'make-appointment.html'{'appointment_form': appointment_form})
