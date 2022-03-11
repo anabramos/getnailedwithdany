ef64b56c0d1975e61f489cb356a38c0ea82d1128
anabramos <anaramosbarretto@gmail.com>
2022-03-11 18:15:27 +0000

    Make DateField appear as date type input format

--- booking/forms.py
+++ booking/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Appointment
 
 
+class DateInput(forms.DateInput):
+    """
+    Custom widget for datefields in form
+    """
+    input_type = 'date'
+
+
 class AppointmentForm(forms.ModelForm):
     """
     Creates Form from Appointment Module
@@ -14,3 +21,6 @@ class AppointmentForm(forms.ModelForm):
         fields = ['service',
                   'appointment_date',
                   'appointment_time']
+        widgets = {
+            'appointment_date': DateInput()
+        }
