73422c6141b3ea9101d9f85d9ef0a0c2130f59bd
anabramos <anaramosbarretto@gmail.com>
2022-03-12 07:40:25 +0000

    Change style contact-us form template

--- website/forms.py
+++ website/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class ContactUsForm(forms.Form):
     """ Contact us form for users to submit messages """
-    name = forms.CharField(label='Your name', max_length=50)
+    yourname = forms.CharField(label='Your name', max_length=50)
     email = forms.EmailField(max_length=50)
-    subject = forms.CharField(label='Subject of your message', max_length=100)
+    subject = forms.CharField(label='Subject', max_length=100)
     message = forms.CharField(widget=forms.Textarea)
