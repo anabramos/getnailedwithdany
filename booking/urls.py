b0545df8779ed3d1c1e969e5345d31bc1b79e93a
anabramos <anaramosbarretto@gmail.com>
2022-03-13 16:55:29 +0000

    Create view for changing appointment functionality

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('make-appointment', views.make_appointment, name='make-appointment'),
     path('my-account', views.my_account, name='my-account'),
+    path('edit/<appointment_appointment_id>', views.change_appointment, name='edit')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from .forms import AppointmentForm
 from .models import Appointment
@@ -30,3 +30,21 @@ def my_account(request):
     current_user = request.user
     my_appointments = Appointment.objects.filter(user=current_user)
     return render(request, 'my-account.html', {'my_appointments': my_appointments})
+
+
+def change_appointment(request, appointment_appointment_id):
+    """
+    Change Appointment details
+    """
+    appointment = get_object_or_404(Appointment,
+                                    appointment_id=appointment_appointment_id)
+    if request.method == "POST":
+        appointment_form = AppointmentForm(request.POST, instance=appointment)
+        if appointment_form.is_valid():
+            appointment = appointment_form.save(commit=False)
+            appointment.user = request.user
+            appointment.save()
+
+    appointment_form = AppointmentForm(instance=appointment)
+    return render(request, 'change-appointment.html',
+                  {'appointment_form': appointment_form})
