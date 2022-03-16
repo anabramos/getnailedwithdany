3cb7d06c5d5cb51b2696fa3d13a69df74c6d92d6
anabramos <anaramosbarretto@gmail.com>
2022-03-16 15:43:50 +0000

    load datetimepicker extension to configure datetime-local input

---
+++ static/js/flatpicker-config.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const currentDate = new Date();
+
+    flatpickr("#id_timestamp", { 
+        // Use flatpickr to configure date availibility
+        // Documentation: https://flatpickr.js.org/
+        // General settings
+        allowInput:		true,
+        theme: 			"material_green",      
+        
+        // Date settings
+        minDate:        "today", 
+        disable:        [
+            function(date) {
+                // return true to disable
+                return (date.getDay() === 0 || date.getDay() === 6);
+            }
+        ],
+        
+        // Time Settings
+        enableTime:     true,
+        time_24hr:      true,
+        minTime:        "10:00",
+        maxTime:        "17:00",
+    });
+});
